a54ae0fb2d441244b3c2505f3730755360fe909a
shreyanskhobare <83969833+shreyanskhobare@users.noreply.github.com>
2023-07-15 06:19:36 +0530

    Added jar which contains AOP based logging at Controller layer

--- src/main/java/com/sk/group/ms/employee/SkGroupMsEmployeeServiceApplication.java
+++ src/main/java/com/sk/group/ms/employee/SkGroupMsEmployeeServiceApplication.java
@@ -3,9 +3,11 @@ package com.sk.group.ms.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan("com.*")
+@EntityScan("com.sk.group.shared.*")
+@ComponentScan("com.sk.group.*")
 public class SkGroupMsEmployeeServiceApplication {
 
 	public static void main(String[] args) {
